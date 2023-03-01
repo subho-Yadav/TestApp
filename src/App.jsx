789dda962ce9be74c432899dbadc3dd0eb151336
subho-Yadav <subhoyadav@gmail.com>
2023-03-01 21:06:19 +0530

    Third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,1 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-     <h1>React app</h1>
-    </div>
-  )
-}
-
-export default App
+My name is Subho
